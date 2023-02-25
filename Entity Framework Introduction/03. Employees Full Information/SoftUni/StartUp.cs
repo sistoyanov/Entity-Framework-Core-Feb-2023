bcaac50b1c864e5e3b4107db762e78eec69ae77c
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-02-25 13:18:33 +0200

    Upload

---
+++ Entity Framework Introduction/03. Employees Full Information/SoftUni/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace SoftUni;
+using System.Text;
+using Data;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetEmployeesFullInformation(dbContext));
+    }
+
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder output = new StringBuilder();
+
+        var employess = context.Employees
+               .OrderBy(e => e.EmployeeId)
+               .Select(e => new
+               {
+                   e.FirstName,
+                   e.LastName,
+                   e.MiddleName,
+                   e.JobTitle,
+                   e.Salary
+               })
+               .ToArray();
+
+        foreach ( var e in employess ) 
+        { 
+          output.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
+
---
+++ Entity Framework Introduction/04. Employees with Salary Over 50 000/SoftUni/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace SoftUni;
+
+using System.Text;
+using Data;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetEmployeesWithSalaryOver50000(dbContext));
+    }
+
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder output = new StringBuilder();
+
+        var employess = context.Employees
+               .Where(e => e.Salary > 50000)
+               .OrderBy(e => e.FirstName)
+               .Select(e => new
+               {
+                   e.FirstName,
+                   e.Salary
+               })
+               .ToArray();
+
+        foreach ( var e in employess ) 
+        { 
+          output.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
+
