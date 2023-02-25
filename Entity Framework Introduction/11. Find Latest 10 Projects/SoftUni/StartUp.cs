40e9929afb67bfe6bcfb32d4dae08fd74a42f867
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-02-25 17:16:35 +0200

    Upload

---
+++ Entity Framework Introduction/11. Find Latest 10 Projects/SoftUni/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace SoftUni;
+
+using System.Globalization;
+using System.Text;
+using Data;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetLatestProjects(dbContext));
+    }
+
+    public static string GetLatestProjects(SoftUniContext context)
+    {
+        StringBuilder output = new StringBuilder();
+
+        var projects = context.Projects
+            .OrderBy(p => p.Name)
+            .Take(10)
+            .Select(p => new
+            {
+               p.Name,
+               p.Description,
+               StartDate = p.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+
+            })
+            .ToArray();
+
+        foreach ( var p in projects) 
+        {
+            output.AppendLine(p.Name)
+                  .AppendLine(p.Description)
+                  .AppendLine(p.StartDate);
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
+
