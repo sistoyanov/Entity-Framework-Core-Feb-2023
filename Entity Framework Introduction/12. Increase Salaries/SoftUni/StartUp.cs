1f0be33543d7760e211ab5dffc24b789db2ba5f7
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-02-25 18:42:40 +0200

    Upload

---
+++ Entity Framework Introduction/12. Increase Salaries/SoftUni/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace SoftUni;
+
+using System.Text;
+using Data;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(IncreaseSalaries(dbContext));
+    }
+
+    public static string IncreaseSalaries(SoftUniContext context)
+    {
+        StringBuilder output = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.Department.Name == "Engineering" || e.Department.Name == "Tool Design" || e.Department.Name == "Marketing" || e.Department.Name == "Information Services")
+            .OrderBy(e => e.FirstName)
+            .ThenBy(e => e.LastName)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                //e.Salary
+               Salary = e.Salary * 1.12m
+
+            })
+            .ToArray();
+
+        //foreach (var employee in employees)
+        //{
+        //    employee.Salary *= 1.12m;
+        //}
+
+        //context.SaveChanges();
+
+        foreach ( var e in employees) 
+        {
+            output.AppendLine($"{e.FirstName} {e.LastName} (${e.Salary:f2})");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
+
---
+++ Entity Framework Introduction/13. Find Employees by First Name Starting With Sa/SoftUni/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SoftUni;
+
+using System.Text;
+using Data;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetEmployeesByFirstNameStartingWithSa(dbContext));
+    }
+
+    public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+    {
+        StringBuilder output = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.FirstName.StartsWith("Sa"))
+            .OrderBy(e => e.FirstName)
+            .ThenBy(e => e.LastName)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.JobTitle,
+                e.Salary
+
+            })
+            .ToArray();
+
+        foreach ( var e in employees) 
+        {
+            output.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle} - (${e.Salary:f2})");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
+
