a45e5460ff93f0fd2f56840f745a30b1e99417a5
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-02-25 14:29:21 +0200

    Upload

---
+++ Entity Framework Introduction/05. Employees from RD/SoftUni/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace SoftUni;
+
+using System.Text;
+using Data;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(GetEmployeesFromResearchAndDevelopment(dbContext));
+    }
+
+    public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+    {
+        StringBuilder output = new StringBuilder();
+
+        var employess = context.Employees
+               .Where(e => e.Department.Name == "Research and Development")
+               .OrderBy(e => e.Salary)
+               .ThenByDescending(e => e.FirstName)
+               .Select(e => new
+               {
+                   e.FirstName,
+                   e.LastName,
+                   Department = e.Department.Name,
+                   e.Salary
+               })
+               .ToArray();
+
+        foreach ( var e in employess ) 
+        { 
+          output.AppendLine($"{e.FirstName} {e.LastName} from {e.Department} - ${e.Salary:f2}");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
+
---
+++ Entity Framework Introduction/06. Adding a New Address and Updating Employee/SoftUni/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace SoftUni;
+
+using System.Text;
+using Data;
+using Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine(AddNewAddressToEmployee(dbContext));
+    }
+
+    public static string AddNewAddressToEmployee(SoftUniContext context)
+    {
+        Address newAdress = new Address()
+        {
+            AddressText = "Vitoshka 15",
+            TownId = 4
+            
+        };
+
+        Employee? searchedEmpoloyee = context.Employees.FirstOrDefault(e => e.LastName == "Nakov");
+        searchedEmpoloyee!.Address = newAdress;
+
+        context.SaveChanges();
+
+        StringBuilder output = new StringBuilder();
+
+        var employess = context.Employees
+               .OrderByDescending(e => e.AddressId)
+               .Take(10)
+               .Select(e => new
+               {
+                   Address = e.Address!.AddressText
+               })
+               .ToArray();
+
+        foreach ( var e in employess ) 
+        { 
+          output.AppendLine($"{e.Address}");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
+
