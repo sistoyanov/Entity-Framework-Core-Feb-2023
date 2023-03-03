711c491cef330f8f3c0c58008ac9c5a01a24af4d
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-03 09:33:00 +0200

    Upload P02_FootballBetting

---
+++ Entity Relations/P02_FootballBetting/P02_FootballBetting/Data/FootballBettingContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P02_FootballBetting.Data;
+
+public class FootballBettingContext : DbContext
+{
+	public FootballBettingContext()
+	{
+
+	}
+
+	public FootballBettingContext(DbContextOptions<FootballBettingContext> options) : base(options)
+	{
+
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+
+	}
+}
