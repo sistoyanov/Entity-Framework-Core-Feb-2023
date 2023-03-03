7a670f12eedd45db152cec457235f2edffdeac69
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-03 17:51:51 +0200

    Upload Advanced Querying

---
+++ Advanced Querying/02. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using BookShop.Data;
+using System.Text;
+
+namespace _02._Age_Restriction;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+       BookShopContext context= new BookShopContext();
+
+        //string input = Console.ReadLine().ToLower();
+        Console.WriteLine(GetBooksByAgeRestriction(context, "Minor"));
+    }
+
+    public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+    {
+        StringBuilder output = new StringBuilder();
+        
+        var booksTitles = context.Books
+                           .ToArray()
+                           .Where(b => b.AgeRestriction.ToString().ToLower() == command)
+                           .Select(b => new
+                           {
+                               b.Title
+                           })
+                           .ToArray();
+
+        foreach ( var book in booksTitles ) 
+        {
+           output.AppendLine(book.Title);
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
