2078094ce6348f8e991e4049b187a7a46efe4003
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-06 09:55:07 +0200

    Upload 05. Not Released In

---
+++ Advanced Querying/05. Not Released In/BookShop/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            BookShopContext context = new BookShopContext();
+
+            Console.WriteLine("Enter release year: ");
+            int year = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine(GetBooksNotReleasedIn(context, year));
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var booksTitles = context.Books
+                               .AsNoTracking()
+                               .Where(b => b.ReleaseDate!.Value.Year != year)
+                               .OrderBy(b => b.BookId)
+                               .Select(b => b.Title)
+                               .ToArray();
+
+            return string.Join(Environment.NewLine, booksTitles);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
