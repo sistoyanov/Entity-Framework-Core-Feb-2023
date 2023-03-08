6528be9b52ffb5187336619489698d82dde78e2d
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-08 15:28:10 +0200

    upload 11. Count Books

---
+++ Advanced Querying/11. Count Books/BookShop/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            BookShopContext context = new BookShopContext();
+
+            Console.WriteLine("Enter lenght: ");
+            int lengthCheck = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountBooks(context, lengthCheck));
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            int count = context.Books
+                               .AsNoTracking()
+                               .Where(b => b.Title.Length > lengthCheck)
+                               .Count();
+
+            return count;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
