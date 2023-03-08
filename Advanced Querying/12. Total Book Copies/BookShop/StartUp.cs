6ccfa06bc100aaa6fa1c5788ab24a741fe7200a8
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-08 15:39:08 +0200

    12. Total Book Copies

---
+++ Advanced Querying/12. Total Book Copies/BookShop/StartUp.cs
@@ -0,0 +1,64 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            BookShopContext context = new BookShopContext();
+
+            Console.WriteLine(CountCopiesByAuthor(context));
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var authors = context.Authors
+                               .AsNoTracking()
+                               .Select(a => new
+                               {
+                                   a.FirstName,
+                                   a.LastName,
+                                   BookCopies = a.Books.Sum(b => b.Copies)
+                               })
+                               .OrderByDescending(a => a.BookCopies)
+                               .ToArray();
+
+            foreach (var author in authors)
+            {
+                output.AppendLine($"{author.FirstName} {author.LastName} - {author.BookCopies}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
