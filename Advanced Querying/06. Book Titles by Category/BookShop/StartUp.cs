983f59ec323868072ebd1e6f7a5f81c35681f350
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-08 13:56:06 +0200

    Upload 06. Book Titles by Category

---
+++ Advanced Querying/06. Book Titles by Category/BookShop/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            BookShopContext context = new BookShopContext();
+
+            Console.WriteLine("Enter categories: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByCategory(context, input));
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] categories = input.ToLower().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var books = context.Books
+                               .AsNoTracking()
+                               .Where(b => b.BookCategories.Any(bc => categories.Contains(bc.Category.Name.ToLower())))
+                               .OrderBy(b => b.Title)
+                               .Select(b => b.Title)
+                               .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
