e4860caf01760e97f5a58634f99fd7ee28c74f05
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-08 15:21:55 +0200

    10. Book Search by Author

---
+++ Advanced Querying/09. Book Search/BookShop/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            BookShopContext context = new BookShopContext();
+
+            Console.WriteLine("Enter string: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetBookTitlesContaining(context, input));
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+
+            var bookTitles = context.Books
+                               .AsNoTracking()
+                               .Where(b => b.Title.ToLower().Contains(input.ToLower()))
+                               .Select(b => b.Title)
+                               .OrderBy(b => b)
+                               .ToArray();
+
+            return string.Join(Environment.NewLine, bookTitles);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Advanced Querying/10. Book Search by Author/BookShop/StartUp.cs
@@ -0,0 +1,68 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            BookShopContext context = new BookShopContext();
+
+            Console.WriteLine("Enter string: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByAuthor(context, input));
+        }
+
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var books = context.Books
+                               .AsNoTracking()
+                               .Where(b => b.Author.LastName.ToLower().StartsWith(input.ToLower()))
+                               .OrderBy(b => b.BookId)
+                               .Select(b => new
+                               {
+                                   b.Title,
+                                   AuthorFirstName = b.Author.FirstName,
+                                   AuthorLastName = b.Author.LastName
+                               })
+                               .ToArray();
+
+            foreach (var book in books)
+            {
+                output.AppendLine($"{book.Title} ({book.AuthorFirstName} {book.AuthorLastName})");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
