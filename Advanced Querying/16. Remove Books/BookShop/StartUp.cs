3aad693e56c7597f6c21d93c0f436060103a8315
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-09 09:51:47 +0200

    Upload  16. Remove Books

---
+++ Advanced Querying/16. Remove Books/BookShop/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            BookShopContext context = new BookShopContext();
+
+            Console.WriteLine(RemoveBooks(context));
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+
+            Book[] booksToRemove = context.Books.Where(b => b.Copies < 4200).ToArray();
+
+            context.RemoveRange(booksToRemove);
+            context.SaveChanges();
+
+            return booksToRemove.Count();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
