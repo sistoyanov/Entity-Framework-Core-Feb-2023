c04c5d88675cc488d2b5bdcd20d296bbf8f391f2
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-09 09:46:00 +0200

    Upload 15. Increase Prices

---
+++ Advanced Querying/15. Increase Prices/BookShop/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            BookShopContext context = new BookShopContext();
+
+            IncreasePrices(context);
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+
+            Book[] books = context.Books.Where(b => b.ReleaseDate!.Value.Year < 2010).ToArray();
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
