631262aa19563404286ad629d141a4c64743875b
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-21 20:48:38 +0200

    Upload 13. Import Sales

---
+++ JavaScript Object Notation - JSON/13. Import Sales/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer;
+
+public class CarDealerProfile : Profile
+{
+    public CarDealerProfile()
+    {
+        //Supplier
+        this.CreateMap<ImportSupplierDTO, Supplier>();
+
+        //Part
+        this.CreateMap<ImportPartDTO, Part>();
+
+        //Car
+        this.CreateMap<ImportCarDTO, Car>();
+
+        //Customer
+        this.CreateMap<importCustomerDTO, Customer>();
+
+        //Sale
+        this.CreateMap<ImportSaleDTO, Sale>();
+          
+    }
+}
---
+++ JavaScript Object Notation - JSON/13. Import Sales/CarDealer/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace CarDealer;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        CarDealerContext context = new CarDealerContext();
+        string inputJson = File.ReadAllText(@"../../../Datasets/sales.json");
+
+        string result = ImportSales(context, inputJson);
+        Console.WriteLine(result);
+    }
+
+    private static IMapper MapperProvider()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+        var mapper = config.CreateMapper();
+
+        return mapper;
+    }
+
+    public static string ImportSales(CarDealerContext context, string inputJson)
+    {
+        ImportSaleDTO[] importSaleDTOs = JsonConvert.DeserializeObject<ImportSaleDTO[]>(inputJson)!;
+        IMapper mapper = MapperProvider();
+
+        Sale[] sales = mapper.Map<Sale[]>(importSaleDTOs);
+        context.Sales.AddRange(sales);
+        context.SaveChanges();
+
+        return $"Successfully imported {importSaleDTOs.Length}.";
+    }
+}
