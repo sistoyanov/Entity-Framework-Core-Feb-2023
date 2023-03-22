3bd2c8eeb0de1c41618eeedf362abf9f3d48e464
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-22 12:49:22 +0200

    Upload 16. Export Local Suppliers

---
+++ JavaScript Object Notation - JSON/16. Export Local Suppliers/CarDealer/DTOs/Export/ExportCustomerDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace CarDealer.DTOs.Export;
+
+[JsonObject]
+public class ExportCustomerDTO
+{
+    [JsonProperty("Name")]
+    public string Name { get; set; } = null!;
+
+    [JsonProperty("BirthDate")]
+    public string BirthDate { get; set; }
+
+    [JsonProperty("IsYoungDriver")]
+    public bool IsYoungDriver { get; set; }
+}
---
+++ JavaScript Object Notation - JSON/16. Export Local Suppliers/CarDealer/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Newtonsoft.Json;
+
+namespace CarDealer;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        CarDealerContext context = new CarDealerContext();
+
+        string result = GetLocalSuppliers(context);
+        Console.WriteLine(result);
+    }
+
+    private static IMapper MapperProvider()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+        var mapper = config.CreateMapper();
+
+        return mapper;
+    }
+
+    public static string GetLocalSuppliers(CarDealerContext context)
+    {
+        IMapper mapper = MapperProvider();
+
+        ExportSupplierDTO[] suppliers = context.Suppliers
+             .AsNoTracking()
+             .Where(s => s.IsImporter != true)
+             .ProjectTo<ExportSupplierDTO>(mapper.ConfigurationProvider)
+             .ToArray();
+
+        //var suppliers = context.Suppliers
+        //    .AsNoTracking()
+        //    .Where(s => s.IsImporter != true)
+        //    .Select(s => new 
+        //    {
+        //        s.Id, 
+        //        s.Name,
+        //        PartsCount = s.Parts.Count()
+        //    })
+        //    .ToArray();
+
+        return JsonConvert.SerializeObject(suppliers, Formatting.Indented);
+    }
+}
