5645f920f3d3d7448bc14ebbfaede6c982008954
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-23 15:48:49 +0200

    Upload 04. Import Categories and Products

---
+++ Extensible Markup Language - XML/03. Import Categories/ProductShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using ProductShop.Utilities;
+
+namespace ProductShop;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        ProductShopContext context = new ProductShopContext();
+        string inputXml = File.ReadAllText(@"../../../Datasets/categories.xml");
+
+        string result = ImportCategories(context, inputXml);
+        Console.WriteLine(result);
+    }
+
+    private static IMapper CreateMapper()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<ProductShopProfile>());
+
+        return config.CreateMapper();
+    }
+
+    public static string ImportCategories(ProductShopContext context, string inputXml)
+    {
+        IMapper mapper = CreateMapper();
+
+        ImportCategoryDTO[] categoryDTOs = XmlHelper.Deserialize<ImportCategoryDTO[]>(inputXml, "Categories");
+
+        Category[] categories = mapper.Map<Category[]>(categoryDTOs);
+
+        context.Categories.AddRange(categories);
+        context.SaveChanges();
+
+        return $"Successfully imported {categories.Length}";
+    }
+}
---
+++ Extensible Markup Language - XML/04. Import Categories and Products/ProductShop/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using ProductShop.Utilities;
+
+namespace ProductShop;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        ProductShopContext context = new ProductShopContext();
+        string inputXml = File.ReadAllText(@"../../../Datasets/categories-products.xml");
+
+        string result = ImportCategoryProducts(context, inputXml);
+        Console.WriteLine(result);
+    }
+
+    private static IMapper CreateMapper()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<ProductShopProfile>());
+
+        return config.CreateMapper();
+    }
+
+    public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+    {
+        IMapper mapper = CreateMapper();
+
+        ImportCategoryProductDTO[] categoryProductDTOs = XmlHelper.Deserialize<ImportCategoryProductDTO[]>(inputXml, "CategoryProducts");
+        ICollection<CategoryProduct> categoryProducts= new HashSet<CategoryProduct>();
+
+        foreach (var categoryProduct in categoryProductDTOs)
+        {
+            if (context.CategoryProducts.Any(cp => cp.CategoryId != categoryProduct.CategoryId) ||
+                context.CategoryProducts.Any(cp => cp.ProductId != categoryProduct.ProductId))
+            {
+                continue;
+            }
+
+            CategoryProduct newCategoryProduct = mapper.Map<CategoryProduct>(categoryProduct);
+            categoryProducts.Add(newCategoryProduct);
+        }
+
+        context.CategoryProducts.AddRange(categoryProducts);
+        context.SaveChanges();
+
+        return $"Successfully imported {categoryProducts.Count}";
+    }
+}
