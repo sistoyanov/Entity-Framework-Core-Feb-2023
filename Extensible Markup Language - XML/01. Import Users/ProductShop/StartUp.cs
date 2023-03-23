3068988334fc755fdd113da49732f0307bfa1db3
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-23 12:36:49 +0200

    Upload 01. Import Users

---
+++ Extensible Markup Language - XML/01. Import Users/ProductShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using ProductShop.Utilities;
+
+namespace ProductShop;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        ProductShopContext context = new ProductShopContext();
+        string inputXml = File.ReadAllText(@"../../../Datasets/users.xml");
+
+        string result = ImportUsers(context, inputXml);
+        Console.WriteLine(result);
+    }
+
+    private static IMapper CreateMapper()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<ProductShopProfile>());
+
+        return config.CreateMapper();
+    }
+
+    public static string ImportUsers(ProductShopContext context, string inputXml)
+    {
+        IMapper mapper = CreateMapper();
+
+        ImportUserDTO[] userDTOs = XmlHelper.Deserialize<ImportUserDTO[]>(inputXml, "Users");
+
+        User[] users = mapper.Map<User[]>(userDTOs);
+
+        context.Users.AddRange(users);
+        context.SaveChanges();
+
+        return $"Successfully imported {users.Length}";
+    }
+}
