59048e347b76c1ddc50e35f281b819c20e08ff97
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-28 18:30:36 +0300

    Upload 01. Model Defition

---
+++ Exam Preparation/C# DB Advanced Exam - 04 April 2021 - TeisterMask/01. Model Defition/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models;
+
+public class Employee
+{
+    public Employee()
+    {
+        this.EmployeesTasks = new HashSet<EmployeeTask>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(40, MinimumLength = 3)]
+    //[RegularExpression(@"^[a-zA-Z0-9]*$")]
+    public string Username { get; set; } = null!;
+
+    [Required]
+    //[EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    //[RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
+    public string Phone { get; set; } = null!;
+
+    public virtual ICollection<EmployeeTask> EmployeesTasks { get; set; } = null!;
+}
