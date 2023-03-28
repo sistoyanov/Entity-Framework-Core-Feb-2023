bd472adea054d05c73824e9b676441f2bc3a59a2
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-28 15:18:22 +0300

    02. Data Import Upload

---
+++ Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/02. Data Import/Theatre/DataProcessor/ImportDto/ImportCastDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Theatre.Data.Models;
+
+namespace Theatre.DataProcessor.ImportDto;
+
+[XmlType("Cast")]
+public class ImportCastDTO
+{
+    [XmlElement("FullName")]
+    [Required]
+    [StringLength(30, MinimumLength = 4)]
+    public string FullName { get; set; } = null!;
+
+    [XmlElement("IsMainCharacter")]
+    [Required]
+    public bool IsMainCharacter { get; set; }
+
+    [XmlElement("PhoneNumber")]
+    [Required]
+    [RegularExpression(@"^\+44-\d{2}-\d{3}-\d{4}$")]
+    public string PhoneNumber { get; set; } = null!;
+
+    [XmlElement("PlayId")]
+    [Required]
+    public int PlayId { get; set; }
+}
---
+++ Exam Preparation/C# DB Advanced Exam - 04 Dec 2021/02. Data Import/Theatre/DataProcessor/ImportDto/ImportPlayDTO.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.DataProcessor.ImportDto;
+
+[XmlType("Play")]
+public class ImportPlayDTO
+{
+    [XmlElement("Title")]
+    [Required]
+    [StringLength(50, MinimumLength = 4)]
+    public string Title { get; set; } = null!;
+
+    [XmlElement("Duration")]
+    [Required]
+    public string Duration { get; set; } = null!;
+
+    [XmlElement("Raiting")]
+    [Required]
+    [Range(0.00, 10.00)]
+    public float Rating { get; set; }
+
+    [XmlElement("Genre")]
+    [Required]
+    public string Genre { get; set; } = null!;
+
+    [XmlElement("Description")]
+    [Required]
+    [StringLength(700)]
+    public string Description { get; set; } = null!;
+
+    [XmlElement("Screenwriter")]
+    [Required]
+    [StringLength(30, MinimumLength = 4)]
+    public string Screenwriter { get; set; } = null!;
+}
