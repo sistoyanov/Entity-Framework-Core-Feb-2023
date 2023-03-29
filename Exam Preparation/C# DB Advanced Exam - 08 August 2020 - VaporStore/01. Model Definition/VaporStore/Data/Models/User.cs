27cef914a0df5b89022fb4561950ea25931639de
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-29 14:03:25 +0300

    Upload 01. Model Definition

---
+++ Exam Preparation/C# DB Advanced Exam - 08 August 2020 - VaporStore/01. Model Definition/VaporStore/Data/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VaporStore.Data.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(20, MinimumLength = 3)]
+    public string Username { get; set; } = null!;
+
+    [Required]
+    //[RegularExpression(@"^[A-Z][a-z]+ [A-Z][a-z]+$")]
+    public string FullName { get; set; } = null!;
+
+    [Required]
+    public string Email { get; set; } = null!;
+    
+    [Required]
+    [Range(3, 103)]
+    public int Age { get; set; }
+
+    public virtual ICollection<Card> Cards { get; set; } = null!;
+}
