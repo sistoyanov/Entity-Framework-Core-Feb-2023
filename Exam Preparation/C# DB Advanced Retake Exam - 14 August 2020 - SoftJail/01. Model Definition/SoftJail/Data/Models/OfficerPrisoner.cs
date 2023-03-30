da6dfaa2a1be180e45732affcf7539269d42a122
Stoyan Stoyanov <si_stoyanov@outlook.com>
2023-03-30 18:07:47 +0300

    Upload 01. Model Definition

---
+++ Exam Preparation/C# DB Advanced Retake Exam - 14 August 2020 - SoftJail/01. Model Definition/SoftJail/Data/Models/OfficerPrisoner.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftJail.Data.Models;
+
+public class OfficerPrisoner
+{
+    [Required]
+    //[ForeignKey(nameof(Prisoner))]
+    public int PrisonerId { get; set; }
+    [Required]
+    public virtual Prisoner Prisoner { get; set; } = null!;
+
+    [Required]
+    //[ForeignKey(nameof(Officer))]
+    public int OfficerId { get; set; }
+    [Required]
+    public virtual Officer Officer { get; set; } = null!;
+}
